08af24c3779e721a743208b7e3e616cda0acb0e4
Matthieu Symoens <matthieu.symoens@gmail.com>
2026-04-16 10:33:38 -0400

    Added redis to dev stack

--- dev/minio-init/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mc alias set minio http://minio:9000 root password
-
-mc admin user add minio example password
-mc mb minio/example
-mc admin policy create minio example /minio-init/example.json
-mc admin policy attach minio example --user=example
