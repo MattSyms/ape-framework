613d67c5ca131c1d440c3cc402f806d4ce300944
Matthieu Symoens <matthieu.symoens@gmail.com>
2026-04-24 09:34:10 -0400

    Updated dependencies

--- framework/bin/package.ts
+++ framework/bin/package.ts
@@ -4,34 +4,12 @@
  *   Package build:
  *     yarn package 0.0.0-dev.0
  */
-import { join } from 'node:path'
 import fs from 'fs-extra'
 
 const [version] = process.argv.slice(2)
 
 if (!version) {
   throw new Error('missing argument <version>')
-}
-
-type Exports = Record<string, { import: { types: string, default: string } }>
-
-const getExports = (dir: string): Exports => {
-  let exp: Exports = {}
-  fs.readdirSync(dir, { withFileTypes: true }).forEach((file) => {
-    const path = join(dir, file.name)
-    if (file.isDirectory()) {
-      exp = { ...exp, ...getExports(path) }
-    } else {
-      const module = path.replace(/^src\/|\.ts$/gu, '')
-      exp[`./${module}`] = {
-        import: {
-          types: `./dist/${module}.d.ts`,
-          default: `./dist/${module}.js`,
-        },
-      }
-    }
-  })
-  return exp
 }
 
 const pkg = fs.readJsonSync('package.json')
@@ -55,8 +33,16 @@ fs.writeJsonSync('build/package.json', {
   engines: pkg.engines,
   dependencies: pkg.dependencies,
   peerDependencies: pkg.peerDependencies,
-  exports: getExports('src'),
+  exports: {
+    './*': {
+      import: {
+        types: './dist/*.d.ts',
+        default: './dist/*.js',
+      },
+    },
+  },
 }, { spaces: 2 })
 
+fs.copySync('src', 'build/src')
 fs.copySync('../LICENSE', 'build/LICENSE')
 fs.copySync('README.md', 'build/README.md')
